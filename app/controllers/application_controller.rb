b3c6ec142cc89fff28dcf1804437cc58c3d42770
Alexander Horvath <tantousha@me.com>
2018-09-14 15:35:49 -0700

    Added basic auth to application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user, password|
+      user == ENV["ADMIN_NAME"] && password == ENV["ADMIN_PASSWORD"]
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
