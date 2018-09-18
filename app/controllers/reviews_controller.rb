736d1a2750d68a4137521187690df42d994ce198
Alexander Horvath <tantousha@me.com>
2018-09-17 23:55:20 -0700

    Added ability to post reviews if signed in.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,18 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = params[:product_id]
+    @review.save!
+    redirect_to "/products/#{params[:product_id]}"
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  
+
   def product_info item
     Product.find(item.product_id)
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
 end
